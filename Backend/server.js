f384d5d8fc8d5b567b2102c4c8922252475fa5d1
Julia Disconzi <judisconzi90@gmail.com>
2024-11-14 12:13:05 +0000

    Algo

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,40 @@
 const express = require('express');
+const cors = require('cors');
+const { OpenAI } = require('openai');
+require('dotenv').config(); 
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY, 
+});
 
 
 app.get('/', (req, res) => {
   res.send('Backend do Serena rodando!');
 });
 
+app.post('/api/call', async (req, res) => {
+  try {
+    const response = await openai.chat.completions.create({
+      model: 'gpt-4o-mini',
+      messages: [{ role: 'user', content: req.body.prompt }],
+      max_tokens: 1024,
+      temperature: 0.8,
+    });
+
+    res.send(response.choices[0].message.content);
+  } catch (error) {
+    console.error("Erro na chamada da API do OpenAI:", error);
+    res.status(500).send({ error: 'Erro ao gerar resposta. Tente novamente mais tarde.' });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
-
-
